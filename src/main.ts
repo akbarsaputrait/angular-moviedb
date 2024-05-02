44aaa59480f525644ac30b1fc197ca966bd1cea6
akbarsaputrait <akbarsaputrait@outlook.com>
2024-05-02 11:29:35 +0700

    Vercel Speed Insight

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,10 @@
 import { bootstrapApplication } from '@angular/platform-browser';
 import { inject } from '@vercel/analytics';
+import { injectSpeedInsights } from '@vercel/speed-insights';
 import { AppComponent } from './app/app.component';
 import { appConfig } from './app/app.config';
 
+injectSpeedInsights();
 inject();
 
 bootstrapApplication(AppComponent, appConfig).catch((err) =>
