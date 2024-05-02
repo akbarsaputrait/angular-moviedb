7ac990447741c82248ea61039f718e3a9114899d
akbarsaputrait <akbarsaputrait@outlook.com>
2024-05-02 11:24:18 +0700

    Vercel Web Analytics

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,9 @@
 import { bootstrapApplication } from '@angular/platform-browser';
-import { appConfig } from './app/app.config';
+import { inject } from '@vercel/analytics';
 import { AppComponent } from './app/app.component';
+import { appConfig } from './app/app.config';
+
+inject();
 
 bootstrapApplication(AppComponent, appConfig).catch((err) =>
   console.error(err)
